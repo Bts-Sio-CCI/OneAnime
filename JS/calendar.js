78f5902fc7ed2d1f3e84d762d1ba8cfa328281c4
Aldwin <elonyx7@gmail.com>
2023-12-10 18:27:28 +0100

    same

--- JS/calendar.js
+++ JS/calendar.js
@@ -3,20 +3,20 @@ function CalendarControl() {
     const calendarControl = {
       localDate: new Date(),
       prevMonthLastDate: null,
-      calWeekDays: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+      calWeekDays: ["Dim.", "Lun.", "Mar.", "Mer.", "Jeu.", "Ven.", "Sam."],
       calMonthName: [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec"
+        "Janvier",
+        "Février",
+        "Mars",
+        "Avril",
+        "Mai",
+        "Juin",
+        "Juillet",
+        "Août",
+        "Septembre",
+        "Octobre",
+        "Novembre",
+        "Décembre"
       ],
       daysInMonth: function (month, year) {
         return new Date(year, month, 0).getDate();
@@ -85,7 +85,7 @@ function CalendarControl() {
           </div>
           <div class="calendar-next"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 128 128"><path fill="#666" d="M38.8 124.2l52.4-52.42L99 64l-7.77-7.78-52.4-52.4-9.8 7.77L81.44 64 29 116.42z"/></svg></a></div>
           </div>
-          <div class="calendar-today-date">Today: 
+          <div class="calendar-today-date">Aujourd'hui: 
             ${calendarControl.calWeekDays[calendarControl.localDate.getDay()]}, 
             ${calendarControl.localDate.getDate()}, 
             ${calendarControl.calMonthName[calendarControl.localDate.getMonth()]} 
