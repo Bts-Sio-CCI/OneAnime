22eec26f431318619fb3e086454eff01022dbacd
Aldwin <elonyx7@gmail.com>
2023-12-12 11:29:06 +0100

    too

---
+++ JS/searchbar.js
@@ -0,0 +1,13 @@
+document.querySelector('#search-bar').addEventListener('keyup', function () {
+    const searchTerm = this.value.toLowerCase();
+    const manga = Array.from(document.querySelectorAll('.liste_Manga'));
+    
+    manga.forEach(liste_Manga => {
+      const mangaName = liste_Manga.textContent.toLowerCase();
+      if (mangaName.includes(searchTerm)) {
+        liste_Manga.style.display = null;
+      } else {
+        liste_Manga.style.display = 'none';
+      }
+    });
+  });
