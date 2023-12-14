3852737074e37d63591fa0f9fa9818103d27c473
Skey30 <skeymath@gmail.com>
2023-12-14 23:07:13 +0100

    affichage calendar

--- JS/calendar.js
+++ JS/calendar.js
@@ -61,8 +61,8 @@ function CalendarControl() {
           <div class="calendar-next"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 128 128"><path fill="#666" d="M38.8 124.2l52.4-52.42L99 64l-7.77-7.78-52.4-52.4-9.8 7.77L81.44 64 29 116.42z"/></svg></a></div>
           </div>
           <div class="calendar-today-date">Aujourd'hui: 
-            ${calendarControl.calWeekDays[calendarControl.localDate.getDay()]}, 
-            ${calendarControl.localDate.getDate()}, 
+            ${calendarControl.calWeekDays[calendarControl.localDate.getDay()]} 
+            ${calendarControl.localDate.getDate()} 
             ${calendarControl.calMonthName[calendarControl.localDate.getMonth()]} 
             ${calendarControl.localDate.getFullYear()}
           </div>
