29f8ad3f714720d6c911e00f1c4d7f2fa875ffeb
Aldwin <elonyx7@gmail.com>
2023-12-17 11:56:19 +0100

    changement class="list-manga" to  id="list-manga"

--- JS/shop.js
+++ JS/shop.js
@@ -1,9 +1,22 @@
-const lesproduits = document.querySelectorAll("#liste_Manga");
-const panier = document.querySelector(".panier");
+// Get all elements with the class "button-shop"
+var addToCartButtons = document.querySelectorAll('.button-shop');
 
-lesproduits.forEach((produit) => {
-    produit.addEventListener("click", (e) => {
-        panier.innerHTML = produit.textContent+"<br>";
-    })
-})
+// Get the shopping cart element
+var shoppingCart = document.getElementById('panier');
 
+// Add a click event listener to each "button-shop"
+addToCartButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        // Clone the parent node (div with class "manga")
+        var clonedItem = this.parentNode.cloneNode(true);
+
+        // Append the cloned item to the shopping cart
+        shoppingCart.appendChild(clonedItem);
+
+        // Optionally, you can remove the button from the cloned item in the cart
+        var clonedButton = clonedItem.querySelector('.button-shop');
+        if (clonedButton) {
+            clonedButton.remove();
+        }
+    });
+});
