e1fe8c680c0af04ff54285bf9071cf6d4331ca1f
Aldwin <elonyx7@gmail.com>
2024-02-05 20:46:46 +0100

    add navbar to memberlist

--- view/memberlist.php
+++ view/memberlist.php
@@ -11,14 +11,13 @@
 </head>
 
 <body>
-    <?php // require_once 'view/components/navbar.html' ?>
+    <?php require_once 'view/components/navbar.html' ?>
     <form class="form-categ" action="" method="post">
         <h2 class="text-center">Recherchez un membre par catégorie</h2>
         <select name="categ-select" id="categ-select">
             <option value="">Aucune catégorie</option>
             <?php foreach($categories as $categorie){ ?>
                 <option value="<?php echo $categorie["idCateg"] ?>"><?php echo $categorie["nomCateg"] ?></option>
-                <!-- <option value="mineur">catégorie -18 ans</option> -->
             <?php } ?>
         </select>
         <input type="submit" value="Envoyez">
